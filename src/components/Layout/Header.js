ff6b1328815346f130a87e973bd496509828d692
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-07 11:51:18 +0200

    feat: add header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,23 @@
+import mealsImage from '../../assets/meals.png';
+
+// styles
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img
+          src={mealsImage}
+          alt='A table fuul of delicious food!'
+        />
+      </div>
+    </>
+  );
+};
+
+export default Header;
