21f25195c170fd08a3b00c802c0d1bc5e96d12b8
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-07 17:13:49 +0200

    feat: close modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,12 +4,12 @@ import HeaderCartButton from './HeaderCartButton';
 // styles
 import classes from './Header.module.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
         <img
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,9 +3,12 @@ import CartIcon from '../Cart/CartIcon';
 // styles
 import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
   return (
-    <button className={classes.button}>
+    <button
+      className={classes.button}
+      onClick={props.onClick}
+    >
       <span className={classes.icon}>
         <CartIcon />
       </span>
