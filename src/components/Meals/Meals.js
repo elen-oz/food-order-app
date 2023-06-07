6fb2afa7124a13e86671fe27842edbd726f40318
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-07 12:17:42 +0200

    feat: add Meals component

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSumary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSumary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
