31bf75f1b0690137d07961f8f7746acc1f1b63dd
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-11 18:59:16 +0200

    feat: handle http error

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,7 +9,7 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [] = useState();
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
@@ -17,6 +17,11 @@ const AvailableMeals = () => {
       const response = await fetch(
         'https://food-order-app-9b4ad-default-rtdb.europe-west1.firebasedatabase.app/meals.json'
       );
+
+      if (!response.ok) {
+        throw new Error('Oops! An error loading data.');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -34,13 +39,24 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
   if (isLoading) {
     return (
       <section>
         <p className={classes.MealsLoading}>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
