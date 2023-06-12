60a31fbf9a58fe7b30cdf0a86da87015fb532773
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-12 12:11:52 +0200

    read form values

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,17 +1,33 @@
+import { useRef } from 'react';
+
 import classes from './Checkout.module.css';
 
 const Checkout = (props) => {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalInputRef = useRef();
+  const cityInputRef = useRef();
+
   const confirmHandler = (event) => {
     event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredStreet = streetInputRef.current.value;
+    const enteredPostal = postalInputRef.current.value;
+    const enteredCity = cityInputRef.current.value;
   };
 
   return (
-    <form onSubmit={confirmHandler}>
+    <form
+      onSubmit={confirmHandler}
+      className={classes.form}
+    >
       <div className={classes.control}>
         <label htmlFor='name'>Your Name</label>
         <input
           type='text'
           id='name'
+          ref={nameInputRef}
         />
       </div>
       <div className={classes.control}>
@@ -19,6 +35,7 @@ const Checkout = (props) => {
         <input
           type='text'
           id='street'
+          ref={streetInputRef}
         />
       </div>
       <div className={classes.control}>
@@ -26,6 +43,7 @@ const Checkout = (props) => {
         <input
           type='text'
           id='postal'
+          ref={postalInputRef}
         />
       </div>
       <div className={classes.control}>
@@ -33,15 +51,18 @@ const Checkout = (props) => {
         <input
           type='text'
           id='city'
+          ref={cityInputRef}
         />
       </div>
-      <button
-        type='button'
-        onClick={props.onCancel}
-      >
-        Cancel
-      </button>
-      <button>Confirm</button>
+      <div className={classes.actions}>
+        <button
+          type='button'
+          onClick={props.onCancel}
+        >
+          Cancel
+        </button>
+        <button className={classes.submit}>Confirm</button>
+      </div>
     </form>
   );
 };
