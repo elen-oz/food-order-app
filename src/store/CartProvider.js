e6bae61a3d226c44906d838af43225aaba28c6fb
Elena Ozerova <ozerova.lenka@gmail.com>
2023-06-12 14:56:57 +0200

    feat: add clear cart after order

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,38 +6,6 @@ const defaultCartState = {
   totalAmount: 0,
 };
 
-// const cartReducer = (state, action) => {
-//   switch (action.type) {
-//     case 'ADD':
-//       const existingItem = state.items.find((item) => item.id === action.payload.id);
-
-//       const updatedItem = existingItem
-//         ? state.items.map((item) =>
-//             item.id === action.payload.id
-//               ? { ...item, amount: item.amount + action.payload.amount }
-//               : item
-//           )
-//         : [...state.items, action.payload];
-//       return {
-//         items: updatedItem,
-//         totalAmount: state.totalAmount + action.payload.amount * action.payload.price,
-//       };
-
-//     case 'REMOVE':
-//       const itemToRemove = state.items.find((item) => item.id === action.payload);
-//       const updatedAmount = itemToRemove.amount === 1 ? 0 : itemToRemove.amount - 1;
-//       const updatedItems = state.items
-//         .map((item) => (item.id === action.payload ? { ...item, amount: updatedAmount } : item))
-//         .filter((item) => item.amount > 0);
-//       return {
-//         items: updatedItems,
-//         totalAmount: state.totalAmount - itemToRemove.price,
-//       };
-//     default:
-//       return defaultCartState;
-//   }
-// };
-
 const cartReducer = (state, action) => {
   let updatedItems;
   switch (action.type) {
@@ -67,6 +35,8 @@ const cartReducer = (state, action) => {
         items: updatedItems,
         totalAmount: state.totalAmount - itemToRemove.price,
       };
+    case 'CLEAR':
+      return defaultCartState;
 
     default:
       return state;
@@ -90,11 +60,16 @@ const CartProvider = (props) => {
     });
   };
 
+  const clearCartHandler = () => {
+    dispatchCartAction({ type: 'CLEAR' });
+  };
+
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
+    clearCart: clearCartHandler,
   };
 
   return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
